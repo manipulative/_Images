b85b3500d763306447e5452716875db7570bd917
manipulative <497705769@qq.com>
2020-01-30 01:26:30 +0800

    2020-01-30_01-26-30

--- _commit.sh
+++ _commit.sh
@@ -1,10 +1,10 @@
 cd /Users/siyuan/OneDrive/function/_Images
 
+read -p "please input .md dictionary:" dictionary
+python3 /Users/siyuan/OneDrive/function/_Images/replace.py $dictionary
+
 time=`date "+%Y-%m-%d_%H-%M-%S"`
 git add --all
 git commit -m "${time}"
 git push origin master
 echo "Finished Push"
-
-read -p "please input .md dictionary:" dictionary
-python3 replace.py $dictionary
