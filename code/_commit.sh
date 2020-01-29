bb9bab261b7575e2ccec74fa30a3b6372ad042f7
manipulative <497705769@qq.com>
2020-01-30 01:45:13 +0800

    'change update code'

--- code/_commit.sh
+++ code/_commit.sh
@@ -1,7 +1,7 @@
 cd /Users/siyuan/OneDrive/function/_Images
 
 read -p "please input .md dictionary:" dictionary
-python3 /Users/siyuan/OneDrive/function/_Images/_replace.py $dictionary
+python3 /Users/siyuan/OneDrive/function/_Images/code/_replace.py $dictionary
 
 time=`date "+%Y-%m-%d_%H-%M-%S"`
 git add --all
--- test.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/siyuan/OneDrive/function/_Images
-
-read -p "please input .md dictionary:" dictionary
-python3 /Users/siyuan/OneDrive/function/_Images/_replace.py $dictionary
